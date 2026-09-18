8f61c906e391d32d765047c03758677d1f860e85
Rev0710 <delarosa.johnrev071004@gmail.com>
2026-09-19 03:06:28 +0800

    Changing Cors for connection of Front end to Back end

--- server/server.js
+++ server/server.js
@@ -107,6 +107,7 @@ app.use(
         origin: [
             "http://localhost:5173",
             "http://127.0.0.1:5173",
+            "https://votara-election-system.vercel.app/",
         ],
         credentials: true,
     })
